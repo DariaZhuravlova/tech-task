3b39f8da1044aca7434f5a12f9e81a83f4ac82d5
DariaZhur <dariazhur89@gmail.com>
2025-09-05 11:59:32 +0300

    init commit

---
+++ src/js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function () {
+  $(".tabs-nav li").on("click", function (e) {
+    e.stopPropagation();
+
+    const tabId = $(this).data("tab");
+    const $dropdown = $("#" + tabId);
+
+    // снять active со всех
+    $(".tabs-nav li").removeClass("active");
+    $(".tab-item").slideUp(200);
+
+    // добавить active на текущий
+    $(this).addClass("active");
+    $dropdown.stop(true, true).slideDown(200);
+  });
+
+  // клик вне — закрыть
+  $(document).on("click", function () {
+    $(".tabs-nav li").removeClass("active");
+    $(".tab-item").slideUp(200);
+  });
+
+  // клик внутри dropdown — не закрывать
+  $(".tab-item").on("click", function (e) {
+    e.stopPropagation();
+  });
+});
+
+
+// swiper
+
+new Swiper(".swiper", {
+    loop: true,
+    // autoplay: {delay: 3000},
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
